eb9b8986d2b4c4f3a4cc42b57b34d7313a367722
jarne09 <jarne.heer1@gmail.com>
2024-09-05 09:36:33 +1200

    images?

--- music_starter.js
+++ music_starter.js
@@ -1,102 +1,77 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+let img
+let firstRun = true
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(255)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+
+if(firstRun){
+  img = loadImage('brickwall.png')
+  firstRun = false
+}
+
+
+//   background(255)
+//   textFont('Verdana'); // please use CSS safe fonts
+//   rectMode(CENTER)
+//   textSize(10)
+//   text(counter,20,20)
+
   
-  // ellipse (200,200,200,200)
-  // ellipse (500,200,200,200)
-  
-  // let mouthSize = map(vocal,0,100,60,350)
-  // rect (350,500,200,mouthSize)
-
-  // let eyeSize = map (bass,0,100,50,100)
-
-  // fill(255)
-  // ellipse (200,200,200,200)
-  // fill(0)
-  // ellipse (200,200,eyeSize)
-
-  // fill(255)
-  // ellipse (500,200,200,200)
-  // fill(0)
-  // ellipse (500,200,eyeSize)
-
-  // fill(255)
-  // let mouthSize = map (vocal,0,100,50,400)
-  // ellipse (350,500,mouthSize,mouthSize/2)
-
-
-
-
-  // let bar_spacing = height / 10;
-  // let bar_height = width / 12;
-  // let bar_pos_x = width / 2;
-
-
-  // // vocal bar is red
-  // fill(200, 0, 0);
-  // rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  // fill(0);
-  // text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
-
-  // // drum bar is green
-  // fill(0, 200, 0);
-  // rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  // fill(0);
-  // text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
-
-  // // bass bar is blue
-  // fill(50, 50, 240);
-  // rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  // fill(0);
-  // text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
-
-  // // other bar is white
-  // fill(200, 200, 200);
-  // rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  // fill(0);
-  // text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  // fill(255, 255, 0);
-
-  // // display "words"
-  // textAlign(CENTER);
-  // textSize(vocal);
-  // text(words, width/2, height/3);
-
-  // textSize(10)
-  // text(counter,20,20)
+//   strokeWeight(20)
+//   stroke(0)
 
 
   
-  strokeWeight(20)
-  stroke(0)
+
+//   var otherMap = map(other,0,100,0,100)
+//   var lengthOfLine = 50
+//   var lineStart = 100
+//   var lineEnd = lineStart+lengthOfLine
 
 
-  var otherMap = map(other,0,100,5,70)
-  var lengthOfLine = 100
-  var lineStart = 100
-  var lineEnd = lineStart+lengthOfLine
-
-  // if(counter<200 && >300){
-  //   stroke(150)
-  // }
-
-
-  for(var i =1; i <= otherMap; i++){
-    var lineStep=i*5
-    line(lineStart,lineStep,lineEnd,lineStep)
-  }
-
-  var drumMap = map(drum,0,100,5,70)
-  var lineStart2 = lineEnd+lengthOfLine
-  var lineEnd2 = lineStart2+lengthOfLine
-  for(var i =1; i <= drumMap; i++){
-    var lineStep=i*5
-    line(lineStart2,lineStep,lineEnd2,lineStep)
-  }
-
+// if (counter<1380){ 
   
+//   //LOOK AT MUSIC HISTORY FOR FUNCTION
+//   for(var i =1; i <= otherMap; i++){
+//     var lineStep=i*5
+//     line(lineStart,lineStep,lineEnd,lineStep)
+//   }
+
+//   var bassMap = map(bass,0,100,0,100)
+//   var lineStart2 = lineEnd+lengthOfLine
+//   var lineEnd2 = lineStart2+lengthOfLine
+//   for(var i =1; i <= bassMap; i++){
+//     var lineStep=i*5
+//     line(lineStart2,lineStep,lineEnd2,lineStep)
+//   }
+// }
+  
+//   fill(0,255,0)
+//   strokeWeight(5)
+//   stroke(255,0,0)
+//   textAlign(LEFT);
+//   textSize(vocal);
+//   text(words, 50, 600);
+
+
+
+// //other and bass
+
+// x=counter
+// var bassline = map(bass,0,100,0,500)
+
+
+
+// // line(x,bassline+760,x+25,bassline+760)
+
+// //LOOP X BACK TO START
+
+// fill(255)
+// beginShape();
+// vertex(0, 700);
+// bezierVertex(bassline, 500, 100, bassline, 400, 700);
+// endShape();
+
+image(img,0,0)
+
 }
